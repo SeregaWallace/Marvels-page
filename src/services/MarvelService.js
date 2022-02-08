70f95267faea3d88eb34091be4be298acb43fbc1
Serega Kordeluk <serega.wallace@gmail.com>
2022-02-08 15:07:28 +0300

    charList update

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -32,6 +32,7 @@ class MarvelService {
                         char.thumbnail.extension,
             homepage: char.urls[0].url,
             wiki: char.urls[1].url,
+            id: char.id,
         }
     }
 }
